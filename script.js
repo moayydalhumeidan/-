60f4e1b4a10f39c39aee4c25ec0f435c4f492b33
MOAYYD <moayyd.alhumeidan@gmail.com>
2026-03-19 21:56:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const input = document.getElementById("nameInput");
-const nameDisplay = document.getElementById("nameDisplay");
 const whatsappBtn = document.getElementById("whatsappBtn");
 
 whatsappBtn.addEventListener("click", () => {
@@ -8,7 +7,7 @@ whatsappBtn.addEventListener("click", () => {
     const message = `عيدكم مبارك 🌙
 الاسم: ${name}
 هذه بطاقة العيد الخاصة بكم:
-https://moayydalhumeidan.github.io/eid-card/
+https://USERNAME.github.io/eid-card/
 كل عام وأنتم بخير 🌟`;
 
     const url = `https://wa.me/?text=${encodeURIComponent(message)}`;
