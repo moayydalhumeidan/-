2450efd3a443f624bfa69ad828f8c70209d554fa
MOAYYD <moayyd.alhumeidan@gmail.com>
2026-03-19 22:13:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const input = document.getElementById("nameInput");
+const nameDisplay = document.getElementById("nameDisplay");
+input.addEventListener("input", () => {
+    nameDisplay.textContent = input.value;
+});
+
+const input = document.getElementById("nameInput");
 const whatsappBtn = document.getElementById("whatsappBtn");
 whatsappBtn.addEventListener("click", () => {
     const name = input.value.trim();
