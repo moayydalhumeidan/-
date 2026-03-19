e48781045d1f409f96859ca1832e353db5e1faed
MOAYYD <moayyd.alhumeidan@gmail.com>
2026-03-19 22:21:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// ربط خانة الاسم
 const input = document.getElementById("nameInput");
+
+// ربط مكان عرض الاسم
 const nameDisplay = document.getElementById("nameDisplay");
 
+// تحديث الاسم على البطاقة مباشرة
 input.addEventListener("input", () => {
     nameDisplay.textContent = input.value;
 });
+
+// زر واتساب
+const whatsappBtn = document.getElementById("whatsappBtn");
+
+whatsappBtn.addEventListener("click", () => {
+    const name = input.value.trim();
